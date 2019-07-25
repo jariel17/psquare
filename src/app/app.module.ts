de1268f3a471e788edb0123b843b6c9b943a2819
aacostatesting <aacosta@testingsoft.com>
2019-07-24 21:38:22 -0500

    25 Agrega detalle y lugares component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,24 +7,31 @@ import { FormsModule } from '@angular/forms';
 import { AgmCoreModule } from '@agm/core';
 import {ResaltarDirective} from './directives/resaltar.directive';
 import { ContarClicksDirective } from './directives/contar-clicks.directive';
-import {Routes} from '@angular/router';
+import {Routes, RouterModule} from '@angular/router';
+import { DetalleComponent } from './detalle/detalle.component';
+import { LugaresComponent } from './lugares/lugares.component';
+
 const appRoutes: Routes = [
-  {path: '', component: AppComponent},
-  {path: 'lugares', component: AppComponent}
+  {path: '', component: LugaresComponent},
+  {path: 'lugares', component: LugaresComponent},
+  {path: 'detalle', component: DetalleComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     ResaltarDirective,
-    ContarClicksDirective
+    ContarClicksDirective,
+    DetalleComponent,
+    LugaresComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyDD24jNbdcnYmR_HlIPoc4-gBuwJu6G5KE'
-    })
+    }),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/lugares/lugares.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lugares',
+  templateUrl: './lugares.component.html'
+})
+export class LugaresComponent {
+  lat = 8.4334766;
+  lng = -82.3799246;
+  title = 'PSquare';
+  lugares: any = [
+    { plan: 'pagado', cercania: 1, distancia: 1, activo: true, nombre: 'Florería la Gardenia' },
+    { plan: 'gratuito', cercania: 1, distancia: 1.8, activo: true, nombre: 'Donas la pasadita' },
+    { plan: 'gratuito', cercania: 2, distancia: 5, activo: true, nombre: 'Veterinaria huellitas felices' },
+    { plan: 'gratuito', cercania: 3, distancia: 10, activo: false, nombre: 'Sushi Suhiroll' },
+    { plan: 'pagado', cercania: 3, distancia: 35, activo: true, nombre: 'Hotel la Gracia' },
+    { plan: 'gratuito', cercania: 3, distancia: 120, activo: false, nombre: 'Zapatería el Clavo' }
+  ];
+  constructor() {
+  }
+}
